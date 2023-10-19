9ceca931b402b1d50e4daa8314dd8443c7c051c6
SadiquaAkthar <142715450+SadiquaAkthar@users.noreply.github.com>
2023-10-19 19:25:09 +0530

    Update task22.py

--- task22.py
+++ task22.py
@@ -17,17 +17,21 @@ class Followers_Following:
 
     #To extract the data and print the values
    def login(self):
-       self.driver.get(self.url)
-       sleep(2)
+       try:
+            self.driver.get(self.url)
+            sleep(2)
 
-       # To extract the data using Absolute Path
-       follower_count= self.driver.find_element(by=By.XPATH, value='/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[2]/section/main/div/header/section/ul/li[2]/button/span/span').text
-       following_count= self.driver.find_element(by=By.XPATH, value='/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[2]/section/main/div/header/section/ul/li[3]/button/span/span').text
-       sleep(2)
-
-       #To print the values
-       print("Followers ", follower_count)
-       print("Following ", following_count)    
+            # To extract the data using Absolute Path
+            follower_count= self.driver.find_element(by=By.XPATH, value='/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[2]/section/main/div/header/section/ul/li[2]/button/span/span').text
+            following_count= self.driver.find_element(by=By.XPATH, value='/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[2]/section/main/div/header/section/ul/li[3]/button/span/span').text
+            sleep(2)
+       except NoSuchElementException as selenium_error:
+            print("Element not found", selenium_error)
+       finally: 
+            #To print the values
+            print("Followers ", follower_count)
+            print("Following ", following_count)   
+            self.driver.quit() 
 
 FF = Followers_Following()
 FF.login()
